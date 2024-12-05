87f580c1834be08ccff347174969de9e86ecc95b
nautbek <nautbek@gmail.com>
2024-12-05 19:34:30 +0300

    Only local.

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"NautbekInfo/handlers/api"
 	"NautbekInfo/handlers/web"
-	"fmt"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -15,25 +13,11 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	var env string
-	if env = os.Getenv("APP_ENV"); env == "" {
-		log.Fatalf("APP_ENV not found.")
-	} else {
-		fmt.Printf("Run in ENV: %s\n", env)
-	}
-
 	http.HandleFunc("/", web.Home)
 	http.HandleFunc("/api/user_open", api.UserOpen)
 
-	switch env {
-	case "production":
-		baseUri := "/etc/letsencrypt/live/crypto-visor.ru/"
-		if err := http.ListenAndServeTLS(":443", baseUri+"cert.pem", baseUri+"privkey.pem", nil); err != nil {
-			log.Fatalf("Ошибка при запуске сервера %v", err)
-		}
-	default:
-		if err := http.ListenAndServe(":123", nil); err != nil {
-			log.Fatalf("Ошибка при запуске сервера %v", err)
-		}
+	if err := http.ListenAndServe(":123", nil); err != nil {
+		log.Fatalf("Ошибка при запуске сервера %v", err)
 	}
+
 }
